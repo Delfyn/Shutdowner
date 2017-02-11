48b4ece6c725d56f2c039e09d2e4377e6082be65
Lukáš Papík <papikdev@gmail.com>
2017-02-11 08:16:45 +0100

    Linux support
    Added support for posix

--- shutdowner.py
+++ shutdowner.py
@@ -9,26 +9,19 @@ from shutdown import *
 
 
 parser = argparse.ArgumentParser(description='Shutdowner')
-# parser.add_argument("-d", "--dependencies", help="Install dependencies", action='store_true')
 parser.add_argument('action', help='shutdown, restart, hibernate, logoff, cancel')
 parser.add_argument('time', help='Time in seconds', type=int)
 parser.add_argument("-f", "--force", help="Force application close", action='store_true')
-parser.add_argument("-w", "--warning", help="Instant off", action='store_true')
+parser.add_argument("-i", "--instant", help="Instant off", action='store_true')
 args = parser.parse_args()
 
 
 def print_delfyn():
     print(
         "                                         __ \n                                     _.-~  ) \n                          _..--~~~~,'   ,-/     _ \n                       .-'. . . .'   ,-','    ,' ) \n                     ,'. . . _   ,--~,-'__..-'  ,' \n                   ,'. . .  (@)' ---~~~~      ,' \n                  /. . . . '~~             ,-' \n                 /. . . . .             ,-' \n                ; . . . .  - .        ,' \n               : . . . .       _     / \n              . . . . .          `-.: \n             . . . ./  - .          ) \n            .  . . |  _____..---.._/ ____ ~ Delfyn ~ ... ... ... " + os.name + " \n      ~---~~~~----~~~~~~---~~~~----~~~ \n")
-# def dependencies():
-#     print("Look's you don't have installed dependencies :( \n")
-#     print("Installing shutdown : \n")
-#     pip.main(["install", "shutdown"])
-#     print("Installing proggressbar2 : \n")
-#     pip.main(["install", "progressbar2"])
 
 
-def select_action(action, time, force=False, instant=False):
+def windows_action(action, time, force=False, instant=False):
     if action == "shutdown":
         shutdown(time=time, force=force, warning_off=instant)
 
@@ -41,8 +34,6 @@ def select_action(action, time, force=False, instant=False):
     elif action == "logoff":
         hibernate(force=force)
 
-    elif action == "cancel":
-        cancel()
     else:
         print(" Shutdown - time, force, instant \n Restart - time, force, instant \n Hibernate - force \n Logoff - force \n")
         print(" Cancel progress bar - Ctrl - C \n")
@@ -55,8 +46,35 @@ def progress_bar(arg_time):
 
 
 def windows():
-    select_action(args.action, args.time, args.force, args.warning)
+    windows_action(args.action, args.time, args.force, args.instant)
     progress_bar(args.time)
+
+
+def windows():
+    windows_action(args.action, args.time, args.force, args.instant)
+    progress_bar(args.time)
+
+
+def linux(action, time, force=False, instant=False):
+    if action == "shutdown":
+        os.system("shutdown -t " + str(time))
+
+    elif action == "restart":
+        if args.force:
+            os.system("reboot -f")
+        else:
+            os.system("reboot")
+
+    elif action == "hibernate":
+        os.system("pm-hibernate")
+
+    elif action == "logoff":
+        os.system("logout")
+
+    else:
+        print(
+            " Shutdown - time \n Restart - force \n Hibernate -  \n Logoff -  \n")
+        print(" Cancel progress bar - Ctrl - C \n")
 
 
 if __name__ == '__main__':
@@ -64,6 +82,10 @@ if __name__ == '__main__':
     try:
         if os.name == 'nt':
             windows()
+        elif os.name == 'posix':
+            pass
+        else:
+            print('Mac OS / Unix not supported')
     except KeyboardInterrupt:
         cancel()
         print('  Action aborted -> User interruption')
